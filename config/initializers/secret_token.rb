6b1ff52747d00f50298f692486d043a7eea8a12a
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-09-01 02:11:55 -0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Turbi::Application.config.secret_token = '2376133d689080baa36497e5d977391b14c0efb682231476fa3fd84c167a6805bef2445109b14e0e336410793728bfea2a4e1e05a97180baead4b32d393f3c8c'
