1a73734f9406baab83e3a11ba79020a8e0c33c11
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-09-01 14:19:25 -0300

    created cancan authorization for project update/edit

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe ProjectsController do
   let(:user) { User.make! }
   let(:project) { Project.make! user: user }
+  let(:user_whitout_project) { User.make! email: "email@xample.com" }
   describe "GET 'index'" do
     it "should returns http success" do
       get 'index'
@@ -37,7 +38,7 @@ describe ProjectsController do
       end
     end
 
-    describe "with sign in" do
+    describe "with signed in" do
       before do
         sign_in user
       end
@@ -45,6 +46,33 @@ describe ProjectsController do
       it "should returns http success" do
         get :edit, id: project.id
         response.should be_success
+      end
+    end
+  end
+
+  describe "auhorizations" do
+    describe "edit" do
+      it "signed in with project owner" do
+        sign_in user
+        get :edit, id: project.id
+        response.should be_success
+      end
+
+      it "signed in without project owner" do
+        sign_in user_whitout_project
+        lambda { get :edit, id: project.id }.should raise_error(CanCan::AccessDenied)
+      end
+    end
+    describe "update" do
+      it "signed in with project owner" do
+        sign_in user
+        put :update, id: project.id, project: project.attributes.delete(:id)
+        response.should redirect_to(project_url(project))
+      end
+
+      it "signed in without project owner" do
+        sign_in user_whitout_project
+        lambda { put :update, id: project.id }.should raise_error(CanCan::AccessDenied)
       end
     end
   end
