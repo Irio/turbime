5ccc74e07761a632431f30533646bc3e3edfac18
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2012-09-01 12:37:27 -0300

    Gambiarra

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,4 +1,8 @@
 class ContentController < ApplicationController
   def home
+    @projects = []
+    0.upto(10) do
+      @projects.push Project.new(name: "NOME", description: "descricao", expires_at: rand(365).days.from_now, goal: rand(99))
+    end
   end
 end
