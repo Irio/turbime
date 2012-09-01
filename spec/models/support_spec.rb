8430d6723dfabcfd30d3bef21730b41b84ef6ce5
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-09-01 18:17:49 -0300

    created spec for association in support model

--- spec/models/support_spec.rb
+++ spec/models/support_spec.rb
@@ -7,4 +7,9 @@ describe Support do
     it{ should validate_presence_of :payment_method }
     it{ should validate_presence_of :payment_token }
   end
+
+  describe "associations" do
+    it { should belong_to :user }
+    it { should belong_to :project }
+  end
 end
