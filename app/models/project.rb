5fa4ec36bc26b100233227cbb2fae5e24bb183b7
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-09-01 13:00:57 -0300

    created scopes for project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,12 @@
 class Project < ActiveRecord::Base
   belongs_to :user
   attr_accessible :about, :code_funded, :description, :expires_at, :goal, :name, :repository, :video, :visible
+
+  # Validates
   validates :name, :headline, :description, :goal, :video, :presence => true
-  validates :repository, :video, format: {with: URI::regexp(%w[http https]), allow_nil: true}
+  validates :repository, :video, format: {with: URI::regexp(%w[http https]), allow_nil: true, allow_blank: true}
+
+  # Scopes
+  scope :visible, where(visible: true)
+  scope :expired, where("expires_at < current_timestamp")
 end
