f08b9a90ce64475136aa83532cb6b3390dbff405
Irio Irineu Musskopf Junior <irio.musskopf@caixadeideias.com.br>
2012-09-01 06:08:17 -0300

    ContentController with home action to make acceptance tests pass

--- spec/acceptance/manage_user.rb
+++ spec/acceptance/manage_user.rb
@@ -4,7 +4,7 @@ feature "Manage user" do
 
   scenario "Create a profile using manual sign up" do
     visit "/"
-    click_on "SIGN_UP"
+    click_on "SIGN UP"
     fill_in "Name", with: "Juquinha"
     fill_in "Email", with: "juquinha@turbi.me"
     fill_in "Password", with: "123123"
@@ -17,13 +17,14 @@ feature "Manage user" do
   scenario "Edit profile without fill github, twitter, facebook and site url" do
     auth_user
     visit "/"
-    click_on "Edit profile"
+    click_on "PROFILE"
 
     fill_in "Name", with: "Juquinha da Rocha"
     fill_in "Email", with: "juquinha@turbi.me"
     fill_in "Password", with: "123123123"
     fill_in "Password confirmation", with: "123123123"
     fill_in "Current password", with: "123123"
+    click_on "Update"
 
     page.should have_content("You updated your account successfully.")
   end
