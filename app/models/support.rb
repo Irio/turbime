ca042c6705c0b623cad3466cfc54da06e761fc54
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-09-01 21:17:39 -0300

    support confirmed is protected

--- app/models/support.rb
+++ app/models/support.rb
@@ -1,6 +1,7 @@
 class Support < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
-  attr_accessible :amount, :confirmed, :payment_method, :payment_token, :translaction_id
+  attr_accessible :amount, :payment_method, :payment_token, :translaction_id
   validates :user, :project, :payment_method, :payment_token, presence: true
+  attr_protected :confirmed
 end
