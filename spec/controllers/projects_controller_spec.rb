77aadd08a4776eec426c9f628045330d407f0a4b
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-09-02 19:07:01 -0300

    fixed spec to redired

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -83,7 +83,7 @@ describe ProjectsController do
                               goal: project.goal,
                               video: project.video}
       new_porject = Project.last
-      response.should redirect_to(root_path)
+      response.should redirect_to(project_url(new_porject))
       new_porject.user.should == user
     end
   end
