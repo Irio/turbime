f90ab34b4cfc9152d514c5f65b2cd51018249243
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-09-01 22:11:07 -0300

    creating more tests for support model and created the 'create_payment_token'

--- spec/models/support_spec.rb
+++ spec/models/support_spec.rb
@@ -4,8 +4,7 @@ describe Support do
   describe "validations" do
     it{ should validate_presence_of :user }
     it{ should validate_presence_of :project }
-    it{ should validate_presence_of :payment_method }
-    it{ should validate_presence_of :payment_token }
+    it{ should validate_presence_of :amount }
   end
 
   describe "associations" do
