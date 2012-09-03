c0ac490d1c87d278fc0eb7b6b28338ee480fe139
Irio Irineu Musskopf Junior <irio.musskopf@caixadeideias.com.br>
2012-09-02 23:23:35 -0300

    Seed projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,63 +1,58 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# encoding: UTF-8
 
-user = User.create(
-  {name: "Juquinha da Rocha", email: "juquinha@turbi.me", password: "123123"}
-)
+User.create([
+  {name: "Juquinha da Rocha", email: "juquinha@turbi.me", password: "123123"},
+  {name: "Irio Irineu Musskopf Junior", email: "iirineu@gmail.com", password: "123123"},
+  {name: "Josemar Luedke", email: "josemarluedke@gmail.com", password: "123123"},
+  {name: "Marcio Marques", email: "marcio@caixadeideias.com.br", password: "123123"},
+  {name: "Sérgio Schnorr", email: "sergio@sergiosj.com", password: "123123"}
+])
 
-0.times do
-  Project.create([
-    {visible: true, user_id: user.id, name: "Project name", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.", expires_at: 2.weeks.from_now, headline: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vi.", video: "http://vimeo.com/1752667", repository: "https://github.com/Irio/mymoip", goal: 200.0},
+Project.create([
+  {visible: true, code_funded: "https://github.com/Irio/ruby-tmdb/tree/api3", user_id: User.all[1].id, name: "Better manager on Rails Girls site", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.", expires_at: 2.weeks.from_now, headline: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viv.", video: "http://vimeo.com/17752439", repository: "https://github.com/plus8star/RailsGirls", goal: 2000.0},
+  {visible: true, user_id: User.all[2].id, name: "Implement MoIP on Catarse", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor.", expires_at: 4.weeks.from_now, headline: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit.", video: "http://vimeo.com/35305036", repository: "https://github.com/plus8star/RailsGirls", goal: 1000.0},
+  {visible: true, user_id: User.all[0].id, name: "Homebrew deserves a integration with Linux", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit.", expires_at: 6.weeks.from_now, headline: "Lorem ipsum dolor sit.", video: "http://vimeo.com/42305030", repository: "https://github.com/mxcl/homebrew", goal: 1000.0},
+  {visible: true, user_id: User.all[0].id, name: "Turbi deserves a lorem", description: "Lorem ipsum dolor sit amet, a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit.", expires_at: 4.weeks.from_now, headline: "Lorem ip sdfsdf sdf dsfsum dolor sit.", video: "http://vimeo.com/48297255", repository: "https://github.com/mxcl/homebrew", goal: 1000.0},
+  {visible: true, user_id: User.all[0].id, name: "Turbi some lorem", description: "Lorem ipsum dolor sit amet, a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit.", expires_at: 4.weeks.from_now, headline: "Lorem ip sdfsdf sdf dsfsum dolor sit.", video: "http://vimeo.com/6563331", repository: "https://github.com/mxcl/homebrew", goal: 20.0},
+  {visible: true, user_id: User.all[0].id, name: "Turbi deserves other lorem", description: "Lorem ipsum dolor sit amet, a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit.", expires_at: 9.weeks.from_now, headline: "Lorem ip sdfsdf sdf dsfsum dolor sit.", video: "http://vimeo.com/15083781", repository: "https://github.com/mxcl/homebrew", goal: 10000.0}
+])
 
-    {visible: true, user_id: user.id, name: "Project name", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.", expires_at: 2.weeks.from_now, headline: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.", video: "http://vimeo.com/1752667", repository: "https://github.com/Irio/mymoip", goal: 200.0},
-
-    {visible: true, user_id: user.id, name: "Project name", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.", expires_at: 2.weeks.from_now, headline: "Porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit am", video: "http://vimeo.com/1752667", repository: "https://github.com/Irio/mymoip", goal: 200.0},
-
-    {visible: true, user_id: user.id, name: "Project name", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.", expires_at: 2.weeks.from_now, headline: "Lorem", video: "http://vimeo.com/1752667", repository: "https://github.com/Irio/mymoip", goal: 200.0}
-  ])
-end
-
-if Rails.env.development?
-  lorem = [
-      "Lordent, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "Loridunt ut labo ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur 12x",
-      "Loridunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur 12x",
-      "dolore magna aliam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur 12x",
-      "Loridunt",
-      "voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "Lorem ipsum dolor sit amet, consectetur ad."
-  ]
-  10.times do
-    Project.create(
-      visible: true,
-      user_id: user.id,
-      name: "Project name",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.",
-      expires_at: rand(70).weeks.from_now + 1.minute,
-      headline: lorem.sample,
-      video: "http://vimeo.com/#{rand(20000000)+1}",
-      repository: "https://github.com/Irio/mymoip",
-      goal: rand(100000).to_f
-    )
-  end
-end
-
-35.times do
-  project = Project.all[rand(Project.all.size)]
+15.times do
+  project = Project.all[0]
   support = Support.create(
-    project_id: project.id, amount: 50, user_id: user.id, terms: "1"
+    project_id: project.id, amount: 500, user_id: User.last.id, terms: "1"
   )
   support.confirm!
 end
 
-Project.all[0..2].each do |p|
-  yesterday = Date.yesterday
-  Delorean.time_travel_to("1 month ago") do
-    p.update_attributes(expires_at: yesterday)
-  end
+20.times do
+  project = Project.all[1]
+  support = Support.create(
+    project_id: project.id, amount: 13, user_id: User.last.id, terms: "1"
+  )
+  support.confirm!
+end
+
+20.times do
+  project = Project.all[2]
+  support = Support.create(
+    project_id: project.id, amount: 500, user_id: User.last.id, terms: "1"
+  )
+  support.confirm!
+end
+
+20.times do
+  project = Project.all[4]
+  support = Support.create(
+    project_id: project.id, amount: 900, user_id: User.last.id, terms: "1"
+  )
+  support.confirm!
+end
+
+yesterday = Date.yesterday
+Delorean.time_travel_to("1 month ago") do
+  projects = Project.all
+  projects[0].update_attributes(expires_at: yesterday)
+  projects[3].update_attributes(expires_at: yesterday)
+  projects[4].update_attributes(expires_at: yesterday)
 end
